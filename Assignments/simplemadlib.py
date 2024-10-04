171617f6889f35ba0f1cd67431f670d10517b0f7
evanv25 <836553@mystma.org>
2024-10-04 09:11:31 -0500

    simplemadlib

--- Assignments/simplemadlib.py
+++ Assignments/simplemadlib.py
@@ -1,6 +1,6 @@
 # Once upon a time, in a deep forest,
 # there lived a orchid fairy . 
-# Every day the orchid fairy would beautifully nourish ordhid plants .
+# Every day the orchid fairy would beautifully nourish ordhid plants.
 # One day, a mortal appeared
 # and flew the other away. 
 # The fairy was small and scared.
