36343bd1cf7c70a60bba0553f9cd1872f74ea138
evanv25 <836553@mystma.org>
2024-10-04 09:24:32 -0500

    animal survey

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,10 +1,12 @@
 "What is your favorite animal?"
->
+>Elephants 
 "Why do you like them?"
->
+>they reprent loyalty, power, wisdom, and fertility 
 "Where do they live?" 
->
+>Africa and Southeast Asia
 "What is a fun fact about them?"
->
+>Elephants have around 150,000 muscle units in their trunk.
 "What color are they?" 
->  
+>Grayish to brown
+
+My favorite animal is Elephants because they represent loyalty, power, wisdom, and fertility. They live in Africa and Asia, and they have around 150,000 muscle units in their trunk. They are a grayish to brown in colour. 
