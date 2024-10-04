95dbced41fbc7791f93a1516eda2e43637cebea8
evanv25 <836553@mystma.org>
2024-10-04 09:08:29 -0500

    top five movies

--- Assignments/simplemadlib.py
+++ Assignments/simplemadlib.py
@@ -1,6 +1,6 @@
-# Once upon a time, in a ,
-# there lived a . 
-# Every day the __ would __.
-# One day, a 
-# and __ the __ away. 
-# The __ was __ and emotion ___.
+# Once upon a time, in a deep forest,
+# there lived a orchid fairy . 
+# Every day the orchid fairy would beautifully nourish ordhid plants .
+# One day, a mortal appeared
+# and flew the other away. 
+# The fairy was small and scared.
